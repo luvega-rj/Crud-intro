bc02a23b56775f9a7bcfe58ef55326be48829a8d
luvega-rj <luvegaronny@gmail.com>
2024-08-06 20:16:33 +0300

    first render

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 def home(request):
     
-    return HttpResponse('Hello world!')
+    # return HttpResponse('Hello world!')
+    return render(request,'webapp/index.html')
