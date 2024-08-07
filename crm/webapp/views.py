91723d68016dc024d76248e99f11c1663be9e8ec
luvega-rj <luvegaronny@gmail.com>
2024-08-07 20:17:01 +0300

    views written

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
+from .forms import CreateUserForm, LoginForm
 def home(request):
     
     # return HttpResponse('Hello world!')
     return render(request,'webapp/index.html')
+
+# register
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+       
+       form = CreateUserForm(request.POST)
+
+       if form.is_valid():
+          
+          form.save()
+
+        #   return redirect('')
+
+    context = {'form' : form}
+
+    return render(request,'webbapp/register.html', context=context)
